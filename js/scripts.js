9b22e4c7c2c8a00fdd8e4c3aac62dd7fd4bc0cfe
Anita Kemp, Neha Malhotra and sofia giordano <anita@>
2019-08-21 16:05:15 -0700

    Add consonant spec that affixes ay for consonants

--- js/scripts.js
+++ js/scripts.js
@@ -3,44 +3,26 @@ $(document).ready(function() {
       event.preventDefault();
 
       var sentenceInput = $("#sentence").val().toLowerCase();
-      var vowels = ('a', 'e', 'i', 'o', 'u')
+      var vowels = ['a', 'e', 'i', 'o', 'u'];
+      var characters = sentenceInput.split(" ");
 
-      var characters = sentenceInput.split(" ")
+
       characters.forEach(function(character) {
-        console.log(character);
-        if (character[0] === 'a'|| character[0] === 'e'||character[0] === 'i'|| character[0] === 'o'|| character[0] === 'u') {
-          $(".output").append(character + 'way ');
-        } else if (character[0] !== vowels) {
-            $(".output").append(character + "ay  ");
+
+          if (character[0] === 'a' || character[0] === 'e' || character[0] === 'i' || character[0] === 'o' || character[0] === 'u') {
+            $(".output").append(character + 'way ');
+            // } else if (character[0] === "q" && character[1] === "u") {
+            //   console.log(character.slice(2).append("quay"));
+            // }
+          } else if (!vowels.includes(character[0])) {
+            var letters = character.split("");
+            letters.push(letters.splice(0, 1)[0]);
+            var consonant = letters.join('')
+            $(".output").append (consonant + "ay ");
+
+
+
         }
-
       });
-
-
-
-      // sentenceInput.forEach(function(sentence) {
-      //
-      //     console.log(characters);
-      //     if (characters[0] === "a") {
-      //       return characters + "way"
-      //       alert(characters);
-      //     }
-      //
-      // });
-
-    // function vowelWord(word) {
-    //   }
-    //
-    // };
-
-    // console.log(vowelWord);
-    //
-    //
-    // $('.output').show(sentenceInput);
-
-
-    if (sentenceInput === /^[a]/g){
-      var newWord = $(sentenceInput).append("way");
-    }
   });
 });
